2f9a0700d7ac19f606a222d09cc7379868b36e3a
lor104 <lauren.rosentzveig@gmail.com>
2017-04-01 17:08:00 -0400

    define initialize and create

--- vampires.rb
+++ vampires.rb
@@ -1,3 +1,18 @@
 class Vampire
 
+  @@coven = []
+
+  def initialize(name)
+    @name = name
+    @age = 0
+    @in_coffin = true
+    @drank_blood_today = nil
+  end
+
+  def self.create(vampire)
+    vampire = Vampire.new(vampire)
+    @@coven << vampire
+  end
+
+
 end
