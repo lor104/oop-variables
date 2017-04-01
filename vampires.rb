458db7ae4439f7c4b7a23be28ffc2400f9222076
lor104 <lauren.rosentzveig@gmail.com>
2017-04-01 17:14:31 -0400

    def drink_blood and go_home

--- vampires.rb
+++ vampires.rb
@@ -6,13 +6,33 @@ class Vampire
     @name = name
     @age = 0
     @in_coffin = true
-    @drank_blood_today = nil
+    @drank_blood_today = false
   end
 
   def self.create(vampire)
     vampire = Vampire.new(vampire)
     @@coven << vampire
   end
+
+  def drink_blood
+    @drank_blood_today = true
+  end
+
+  def go_home
+    @in_coffin = true
+  end
+
+  def self.all
+    @@coven
+  end
+
+  #sunrise removed from the coven any vampires who are out of their coffins or who havent drank any blood
+
+
+
+  #sunset sets drank blood today and in coffin to false for the entire coven as they go out in search of blood
+
+
 
 
 end
