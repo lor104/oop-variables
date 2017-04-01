3489ef54b3927ea51e3c1940c01ed43f82671774
lor104 <lauren.rosentzveig@gmail.com>
2017-04-01 17:23:40 -0400

    def hunt and sunset

--- vampires.rb
+++ vampires.rb
@@ -26,6 +26,23 @@ class Vampire
     @@coven
   end
 
+  def self.sunset
+    @@coven.each do |vampire|
+      @in_coffin = false
+      @drank_blood_today = false
+    end
+  end
+
+  def self.hunt
+    @@coven.each do |vampire|
+      if rand(3) == 1 #(0, 1, or 2: 33% chance they'll feed)
+        vampire.drink_blood
+      end
+    end
+  end
+
+
+
   #sunrise removed from the coven any vampires who are out of their coffins or who havent drank any blood
 
 
